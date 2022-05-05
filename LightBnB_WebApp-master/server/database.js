5b845494e857036794b0d7df3f4443f485265a6e
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-05 09:30:15 -0700

    Add get my reservations query and render the reservations on client

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -84,7 +84,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guestId, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT properties.*, reservations.id, reservations.start_date, ROUND(AVG(rating),2) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guestId, limit])
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })  .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
